8fc84c969ff2d3af92afb5c20f5f486c30d16516
reggyshicky <reginahshikanda@gmail.com>
2024-01-11 08:20:32 +0300

    obfuscation implementation

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -9,9 +9,6 @@ import os
 import mysql.connector
 
 
-PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
-
-
 def filter_datum(fields: List[str], redaction: str,
                  message: str, seperator: str) -> str:
     """
