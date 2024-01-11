cbec57e6ba5851921c14202c0187f55b6f767001
reggyshicky <reginahshikanda@gmail.com>
2024-01-11 08:43:08 +0300

    obfuscation implementation

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,7 +7,7 @@ import re
 
 
 def filter_datum(fields: List[str], redaction: str,
-                 message: str, seperator: str) -> str:
+                 message: str, separator: str) -> str:
     """
     Returns an obfuscated log message
     """
