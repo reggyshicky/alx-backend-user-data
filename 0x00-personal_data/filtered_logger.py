cadbae0a4ebab8e1ad9c430e35ffc16cb0e65528
reggyshicky <reginahshikanda@gmail.com>
2024-01-11 14:48:15 +0300

    Implement a get_db function that returns a connector to the database

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -73,7 +73,7 @@ def get_logger() -> logging.Logger:
 
 def get_db() -> mysql.connector.connection.MySQLConnection:
     """
-    mysql connector object
+
     """
     user = os.getenv('PERSONAL_DATA_DB_USERNAME') or "root"
     passwd = os.getenv('PERSONAL_DATA_DB_PASSWORD') or ""
