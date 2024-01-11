d9f55e55291a26e06ad01309254f5238280694d0
reggyshicky <reginahshikanda@gmail.com>
2024-01-11 05:09:39 +0300

    obfuscation implementation

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Definition of filter_datum function that returns an obsfuscated
+log message
+"""
+from typing import List
+import re
+import logging
+import os
+import mysql.connector
+
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, seperator: str) -> str:
+    """
+    Return an obfuscated log message
+    """
+    for field in fields:
+        message = re.sub(field+'=.*?'+seperator,
+                         field+'='+redaction+seperator, message)
+        return message
