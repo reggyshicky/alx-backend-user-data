d3df3e20f2a79597728ec887d6bb640bcb5b0113
reggyshicky <reginahshikanda@gmail.com>
2024-01-16 08:35:00 +0300

    created a class called auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -3,17 +3,18 @@
 from flask import request
 from typing import List, TypeVar
 
+
 class Auth():
     """Module for aunthentication"""
-    
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """returns True if the path is not in excluded_paths"""
         return False
-    
+
     def authorization_header(self, request=None) -> str:
         """validates all requests to secure the API"""
         return None
-    
+
     def current_user(self, requst=None) -> TypeVar('User'):
         """Validates all requests to secure the API"""
         return None
