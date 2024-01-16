8bdf2b686f150c96d0f25580bf46d3276caa6c3a
reggyshicky <reginahshikanda@gmail.com>
2024-01-16 07:37:46 +0300

    created a class called auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -6,10 +6,13 @@ class Auth():
     """Module for aunthentication"""
     
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """returns True if the path is not in excluded_paths"""
         return False
     
     def authorization_header(self, request=None) -> str:
+        """validates all requests to secure the API"""
         return None
     
     def current_user(self, requst=None) -> TypeVar('User'):
+        """Validates all requests to secure the API"""
         return None
