9ad146a09c79b8dab42baff354717cb816cebc88
reggyshicky <reginahshikanda@gmail.com>
2024-01-25 03:49:21 +0300

    creating a new user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -14,3 +14,43 @@ from user import User
 def _hash_password(password: str) -> bytes:
     """Hashes a password"""
     return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+
+def _generate_uuid() -> str:
+    """Generate a UUID"""
+    return str(uuid4())
+
+
+class Auth:
+    """
+    Auth class to interact with authentication database
+    """
+    def __init__(self):
+        """
+        Initializes a new Auth instance
+        """
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+        Adds a new user to the database
+        """
+        try:
+            self._db.find_user_by(email=email)
+        except NoResultFound:
+            return self._db.add_user(email, _hash_password(password))
+        raise ValueError("User {} already exists".format(email))
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Checks if a user's login details are valid"""
+        user = None
+        try:
+            user = self._db.find_user_by(email=email)
+            if user is None:
+                return bcrypt.checkpw(
+                        password.encode("utf-8"),
+                        user.hashed_password,
+                        )
+        except NoResultFound:
+            return False
+        return False
