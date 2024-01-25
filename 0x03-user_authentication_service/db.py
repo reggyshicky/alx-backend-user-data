c6544832c544c36ec1368699a8c86f7205267b65
reggyshicky <reginahshikanda@gmail.com>
2024-01-25 03:08:11 +0300

    implement the DB.find_user_by method. This method takes in arbitrary keyword arguments and returns the first row found in the users table

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -2,7 +2,7 @@
 """
 DB module
 """
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, tuple_
 from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -43,3 +43,21 @@ class DB:
             self._session.rollback()
             new_user = None
         return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """
+        Finds a user based on a set of filters
+        """
+        fields, values = [], []
+        for key, value in kwargs.items():
+            if hasattr(User, key):
+                fields.append(getattr(User, key))
+                values.append(value)
+            else:
+                raise InvalidRequestError()
+        result = self._session.query(User).filter(
+                tuple_(*fields).in_([tuple(values)])
+                ).first()
+        if result is None:
+            raise NoResultFound()
+        return result
