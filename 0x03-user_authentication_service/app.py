f9398625e1320d743a4caf236cb34575fbc21f6f
reggyshicky <reginahshikanda@gmail.com>
2024-01-25 04:38:12 +0300

    mplement a login function to respond to the POST /sessions route.

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -37,7 +37,7 @@ def login() -> str:
     session_id = AUTH.create_session(email)
     response = jsonify({"email": email, "message": "logged in"})
     response.set_cookie("session_id", session_id)
-    return respnose
+    return response
 
 
 @app.route("/sessions", methods=["DELETE"], strict_slashes=False)
