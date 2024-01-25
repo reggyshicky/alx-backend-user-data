e0d489f2270b4375d872ca6f3c868da44080d458
reggyshicky <reginahshikanda@gmail.com>
2024-01-25 04:07:19 +0300

    register uses

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -17,9 +17,16 @@ def inde() -> str:
     return jsonify({"message": "Bienvenue"})
 
 
-# @app.route("/users", methods=["POST"], strict_slashes=False)
-# def users() -> str:
-#    """returns the ac
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def users() -> str:
+    """register uses"""
+    email, password = request.form.get("email"), request.form.get("password")
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
