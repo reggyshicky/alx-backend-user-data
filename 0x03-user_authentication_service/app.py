929b3f7593540b97d348d9804c1a4d5594731f43
reggyshicky <reginahshikanda@gmail.com>
2024-01-25 05:24:31 +0300

    reset password

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -58,7 +58,7 @@ def profile() -> str:
     user = AUTH.get_user_from_session_id(session_id)
     if user is None:
         abort(403)
-    return jsonify({"email": user.email), 200
+    return jsonify({"email": user.email}), 200
 
 
 @app.route("/reset_password", methods=["POST"], strict_slashes=False)
